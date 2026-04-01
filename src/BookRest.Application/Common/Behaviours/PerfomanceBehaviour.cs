45d0e33d60a7eff9047efbd405eae20d7dd4dcc3
dmshved <d.shved.wrk@gmail.com>
2026-04-01 20:03:54 +0300

    feat: add PerfomanceBehaviour

---
+++ src/BookRest.Application/Common/Behaviours/PerfomanceBehaviour.cs
@@ -0,0 +1,46 @@
+﻿// Pipeline behaviour that measures the execution time of a request handler.
+// - Starts a timer before invoking the next delegate in the pipeline
+// - Logs a warning with request name and user information if the request takes longer than 500 ms
+
+using BookRest.Application.Common.Interfaces;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace BookRest.Application.Common.Behaviours;
+
+public class PerfomanceBehaviour<TRequest, TResponse>(ILogger<TRequest> logger, IUser user, IIdentityService identityService) : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull
+{
+    private readonly Stopwatch _timer = new Stopwatch();
+    private readonly ILogger<TRequest> _logger = logger;
+    private readonly IUser _user = user;
+    private readonly IIdentityService _identityService = identityService;
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        _timer.Start();
+
+        var response = await next();
+
+        _timer.Stop();
+
+        var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+        if (elapsedMilliseconds > 500)
+        {
+            var reqestName = typeof(TRequest).Name;
+            var userId = _user.Id ?? string.Empty;
+            var userName = string.Empty;
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                userName = await _identityService.GetUserNameAsync(userId);
+            }
+
+            _logger.LogWarning("BookRest Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@UserId} {@UserName} {@Request}",
+                reqestName, elapsedMilliseconds, userId, userName, request);
+        }
+
+        return response;
+    }
+}
