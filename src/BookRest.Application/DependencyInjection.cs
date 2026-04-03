5b8fd134078139f75ef7f93bd6f89697ebd2f4fe
dmshved <d.shved.wrk@gmail.com>
2026-04-03 21:22:18 +0300

    feat: add dependency injection setup for Application

---
+++ src/BookRest.Application/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using BookRest.Application.Common.Behaviours;
+using Microsoft.Extensions.Hosting;
+
+// Placing the extension method in this namespace makes it 
+// avaliable without requiring an additional using.
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        // TODO: Register AutoMapper profiles
+        // TODO: Register FluentValidation validators
+
+        builder.Services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddOpenRequestPreProcessor(typeof(LoggingBehaviour<>));
+            cfg.AddOpenBehavior(typeof(UnhandledExceptionBehaviour<,>));
+            cfg.AddOpenBehavior(typeof(AuthorizationBehaviour<,>)); 
+            cfg.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+            cfg.AddOpenBehavior(typeof(PerfomanceBehaviour<,>));
+        });
+    }
+}
