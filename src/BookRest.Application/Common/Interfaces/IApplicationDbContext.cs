1834fd3703cb46bcc71923da650d53c6d5752b53
dmshved <d.shved.wrk@gmail.com>
2026-04-09 18:10:20 +0200

    chore: add refresh token entity into Application IApplicationDbContext

--- src/BookRest.Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/BookRest.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,6 +1,10 @@
+using BookRest.Domain.Entities;
+
 namespace BookRest.Application.Common.Interfaces;
 
 interface IApplicationDbContext
 {
+    DbSet<RefreshToken> RefreshTokens { get; }
+
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
