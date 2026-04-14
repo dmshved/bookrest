dd269e81f0c845f67734a14f6d20253abcec9abc
dmshved <d.shved.wrk@gmail.com>
2026-04-14 19:00:43 +0200

    feat: add DispatchDomainEventsInterceptor

---
+++ src/BookRest.Infrastructure/Data/Interceptors/DispatchDomainEventsInterceptor.cs
@@ -0,0 +1,46 @@
+﻿// Dispatches all Domain events before EF Core saves changes to the database.
+
+using BookRest.Domain.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace BookRest.Infrastructure.Data.Interceptors;
+
+internal class DispatchDomainEventsInterceptor(IMediator mediator) : SaveChangesInterceptor
+{
+    private readonly IMediator _mediator = mediator;
+
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        DispatchDomainEvents(eventData.Context).GetAwaiter().GetResult();
+
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        await DispatchDomainEvents(eventData.Context);
+
+        return await base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    public async Task DispatchDomainEvents(DbContext? context)
+    {
+        if (context is null) return;
+
+        var entities = context.ChangeTracker
+            .Entries<BaseEntity<string>>()
+            .Where(e => e.Entity.DomainEvents.Any())
+            .Select(e => e.Entity);
+
+        var domainEvents = entities
+            .Where(e => e.DomainEvents.Any())
+            .ToList();
+
+        entities.ToList().ForEach(e => e.ClearDomainEvents());
+
+        foreach (var domainEvent in domainEvents)
+            await _mediator.Publish(domainEvent);
+    }
+}
