8a29fbcb86dba80c11f3155f03c1d5b279874da2
dmshved <d.shved.wrk@gmail.com>
2026-04-16 19:20:26 +0200

    refactor: remove outdated version of BookRest.Api
    Removed outdated & unstructured version of BookRest.Api layer.

--- src/BookRest.Api/Configuration/JwtConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookRest.Api.Configuration;
-
-public record class JwtConfiguration
-{
-    public required string Issuer { get; init; }
-    public required string Audience { get; init; }
-    public required double ExpirationInMinutes { get; init; }
-    public required string SecretKey { get; init; }
-}
