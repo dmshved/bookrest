64aa4ea8ee0ffdb4cdd93e8784b1a5e7101422e2
dmshved <d.shved.wrk@gmail.com>
2026-04-20 21:05:40 +0200

    feat: add ApplicationDbContext implementation in the Infrastructure layer

--- src/BookRest.Infrastructure/Data/ApplicationDbContext.cs
+++ src/BookRest.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,21 @@
-﻿namespace BookRest.Infrastructure.Data;
+﻿using BookRest.Application.Common.Interfaces;
+using BookRest.Domain.Entities;
+using BookRest.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
-internal class ApplicationDbContext
+namespace BookRest.Infrastructure.Data;
+
+internal class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IApplicationDbContext
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<RefreshToken> RefreshTokens => Set<RefreshToken>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
