e236f8bcc4486eac00ace2438990876f7adf2f95
dmshved <d.shved.wrk@gmail.com>
2026-05-01 19:09:44 +0200

    feat: add Shared project to centralize service name constants

---
+++ src/BookRest.Shared/Services.cs
@@ -0,0 +1,28 @@
+﻿namespace BookRest.Shared;
+
+public static class Services
+{
+    /// <summary>
+    /// The name of the Database.
+    /// This is the name of the database that will be created and used by the application.
+    /// </summary>
+    public const string Database = "BookRestDb";
+
+    /// <summary>
+    /// The value of the JWT Issuer.
+    /// This is the value of the JWT configuration that will be used by the application.
+    /// </summary>
+    public const string JwtIssuer = "Jwt:Issuer";
+    
+    /// <summary>
+    /// The value of the JWT Audience.
+    /// This is the value of the JWT configuration that will be used by the application.
+    /// </summary>
+    public const string JwtAudience = "Jwt:Audience";
+    
+    /// <summary>
+    /// The value of the JWT SecretKey.
+    /// This is the value of the JWT configuration that will be used by the application.
+    /// </summary>
+    public const string JwtSecretKey = "Jwt:SecretKey";
+}
