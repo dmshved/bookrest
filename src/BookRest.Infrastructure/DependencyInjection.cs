f3e4d0b11a16056e4bf72f31fc7aefabda901e12
dmshved <d.shved.wrk@gmail.com>
2026-05-01 19:10:29 +0200

    feat: add Infrastructure DI IoC Container

---
+++ src/BookRest.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,63 @@
+﻿using BookRest.Infrastructure.Data;
+using BookRest.Infrastructure.Data.Interceptors;
+using BookRest.Application.Common.Interfaces;
+using BookRest.Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BookRest.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructureServices(this IHostApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString(Services.Database);
+
+        Guard.Against.Null(connectionString, message: $"Connection string '{Services.Database}' not found");
+
+        builder.Services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        builder.Services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        builder.Services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseNpgsql(connectionString);
+            // The early development configuration, remove at the Late Development stage
+            options.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+        });
+
+        builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+        builder.Services.AddScoped<ApplicationDbContextInitialiser>();
+
+        builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters.ValidIssuer = builder.Configuration[Services.JwtIssuer];
+                options.TokenValidationParameters.ValidAudience = builder.Configuration[Services.JwtAudience];
+                options.TokenValidationParameters.IssuerSigningKey =
+                    new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration[Services.JwtSecretKey]!));
+            });
+
+        builder.Services.AddAuthorizationBuilder(); 
+        
+        builder.Services
+            .AddIdentityCore<ApplicationUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddApiEndpoints();
+
+        builder.Services.AddSingleton(TimeProvider.System);
+        builder.Services.AddTransient<IdentityService>();
+    }
+}
--- src/BookRest.Infrastructure/GlobalUsings.cs
+++ src/BookRest.Infrastructure/GlobalUsings.cs
@@ -1,1 +1,2 @@
- 
+global using BookRest.Shared; 
+global using Ardalis.GuardClauses; 
