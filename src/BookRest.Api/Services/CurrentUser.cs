6ffec27b6a5d27e4c24a65f9ca3d2dea58c02449
dmshved <d.shved.wrk@gmail.com>
2026-05-08 21:19:28 +0200

    feat: update Api layer: appsettings, launchsettings, add CurrentUser service, add wwwroot/favicon

---
+++ src/BookRest.Api/Services/CurrentUser.cs
@@ -0,0 +1,17 @@
+using System.Security.Claims;
+using BookRest.Application.Common.Interfaces;
+
+namespace BookRest.Api.Services;
+
+public class CurrentUser : IUser
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentUser(IHttpContextAccessor httpContextaccessor)
+    {
+        _httpContextAccessor = httpContextaccessor;
+    }
+
+    public string? Id => _httpContextAccessor?.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+    public List<string>? Roles => _httpContextAccessor?.HttpContext?.User?.FindAll(ClaimTypes.Role).Select(x => x.Value).ToList();
+}
