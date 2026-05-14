303d3554e3ab6587a18ef9d6a97145c71950ac9f
dmshved <d.shved.wrk@gmail.com>
2026-05-14 21:57:22 +0200

    refactor: remove redundant service name constants

--- src/BookRest.Shared/Services.cs
+++ src/BookRest.Shared/Services.cs
@@ -7,22 +7,4 @@ public static class Services
     /// This is the name of the database that will be created and used by the application.
     /// </summary>
     public const string Database = "BookRestDb";
-
-    /// <summary>
-    /// The value of the JWT Issuer.
-    /// This is the value of the JWT configuration that will be used by the application.
-    /// </summary>
-    public const string JwtIssuer = "Jwt:Issuer";
-    
-    /// <summary>
-    /// The value of the JWT Audience.
-    /// This is the value of the JWT configuration that will be used by the application.
-    /// </summary>
-    public const string JwtAudience = "Jwt:Audience";
-    
-    /// <summary>
-    /// The value of the JWT SecretKey.
-    /// This is the value of the JWT configuration that will be used by the application.
-    /// </summary>
-    public const string JwtSecretKey = "Jwt:SecretKey";
 }
